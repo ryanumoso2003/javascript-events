6a00bade38343c42a4e1905668cf9511f9aa317c
Ryan Umoso <ryanumoso04@gmail.com>
2025-04-02 21:43:27 -0400

    Added keyboard event handling

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// MOUSE EVENTS
+// mouse events
 const mouseBtn = document.getElementById('mouseBtn');
 const mouseMsg = document.getElementById('mouseMsg');
 
@@ -8,4 +8,12 @@ mouseBtn.addEventListener('mouseover', () => {
 
 mouseBtn.addEventListener('mouseout', () => {
   mouseMsg.textContent = 'Mouse has left the button!';
+});
+  
+// keyboard events
+const keyInput = document.getElementById('keyInput');
+const keyMsg = document.getElementById('keyMsg');
+
+keyInput.addEventListener('keydown', (event) => {
+  keyMsg.textContent = `You pressed: ${event.key}`;
 });
